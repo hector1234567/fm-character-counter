59cc8285eaea9ba88620c266fe6d6bb9f89ab386
Hector B <hectorbg303@gmail.com>
2025-05-15 20:12:22 +0200

    Form error

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const textContainer = document.getElementById("text-container");
-
+const formErrorMessage = document.querySelector(".form__error-message");
 const charCount = document.getElementById("char-count");
 const wordCount = document.getElementById("word-count");
 const sentenceCount = document.getElementById("sentence-count");
-
 const checkExcludeSpaces = document.getElementById("exclude-spaces");
+const checkCharLimit = document.getElementById("check-char-limit");
+const inputMaxWords = document.getElementById("max-words");
+const timeMessage = document.querySelector(".form__time-message");
 
 function init() {
   textContainer.addEventListener("input", handleTextInput);
   checkExcludeSpaces.addEventListener("change", handleTextInput);
+  checkCharLimit.addEventListener("change", handleSetLimitChars);
+  inputMaxWords.addEventListener("focusout", handleChangeCharsLimit);
 }
 init();
 
@@ -27,10 +31,34 @@ function numberOfChars(text) {
 }
 
 function numberOfWords(text) {
-  return (text.match(/\b\w+\b/g) || []).length;
+  const words = (text.match(/\b\w+\b/g) || []).length;
+  setMinutesOfReadingByWords(words);
+  return words;
 }
 
 function numberOfSentences(text) {
   const sentences = text.match(/[^.!?]+[.!?]/g);
   return sentences ? sentences.length : 0;
+}
+
+function handleSetLimitChars() {
+  if (checkCharLimit.checked) {
+    inputMaxWords.focus();
+    textContainer.setAttribute("maxlength", inputMaxWords.value);
+  } else {
+    textContainer.removeAttribute("maxlength");
+  }
+}
+
+function handleChangeCharsLimit() {
+  textContainer.setAttribute("maxlength", inputMaxWords.value);
+  formErrorMessage.innerHTML = `&#9432; Limit reached! Your text exceeds ${Number(
+    inputMaxWords.value
+  )} characters.`;
+}
+
+function setMinutesOfReadingByWords(words) {
+  timeMessage.innerText = `Approx. reading time: ${Math.floor(
+    words / 400
+  )} minutes`;
 }
